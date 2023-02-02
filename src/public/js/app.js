251d095e3396956c7ed22962cba04d4ecd521bb3
eunhee Ahn <4off4@naver.com>
2023-02-02 17:57:17 +0900

    [20230202] #1.9 Conclusions

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,58 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
 
-alert("hi?");
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);     // JSON Oject를 string으로 변환 
+}
+
+/* 백엔드 -> 프론트엔드로 메시지 보내기 */
+// function 메시지 정리
+function handleOpen() {
+    console.log("Connected to Server!");
+}
+
+function handleClose() {
+    console.log("Disconnected from server!");
+}
+
+// 서버와 연결 되었을 때 발생 
+socket.addEventListener("open", handleOpen);
+
+// 서버로부터 메시지를 받을 때 발생 
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerHTML = message.data;
+    messageList.append(li);
+});
+
+// 서버와 연결이 끊겼을 때 발생 
+socket.addEventListener("close", handleClose);
+
+// 프론트 메시지 
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+
+    // 백엔드로 input값 전송 
+    socket.send(makeMessage("new_message", input.value));
+
+    // socket으로 보낸 후 input 초기화  
+    input.value = "";
+}
+messageForm.addEventListener("submit", handleSubmit);
+
+// 프론트 닉네임 
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+
+    // 백엔드로 전송 
+    socket.send(makeMessage("nickName", input.value));
+
+    // socket으로 보낸 후 input 초기화  
+    input.value = "";
+}
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import http from "http";
+import { type } from "os";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -12,10 +15,63 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 // render 설정 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
-// chatAll url 사용 (url 뒤에 아무 오타가 나도 home으로 리다이렉트)
-app.get("/*", (req, res) => res.redirect("/"));
+// chachall url 사용 (url 뒤에 아무 오타가 나도 home으로 리다이렉트)
+app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// http와 ws 프로토콜 2개를 모두 합칠 예정 
+const handleListen = () => console.log(`Listening on http://localhost:3000`); //app.listen(3000, handleListen);
+
+/* http 서버 위에 ws 서버를 만들기 */
+// http 프로토콜 서버 
+const server = http.createServer(app);
+// websocket 프로토콜 서버 
+const wss = new WebSocket.Server({ server });
+
+// function 메시지 정리
+function onSocketClose(){
+    console.log("Disconnected from the Browser!");
+};
+
+// 누군가 서버에 연결하면 그 커넥션을 넣음 
+const sockets = [];
+
+// on 메서드는 백엔드에서 연결된 사람의 정보를 제공해준다, 코드는 2번 작동한다 
+wss.on("connection", (socket)=> {
+    sockets.push(socket);
+
+    // 익명의 닉네임을 가진 사람을 정의 
+    socket["nickName"] = "Anonymous";
+
+    // 브라우저 연결 
+    console.log("Connected to Browser!");
+
+    // 브라우저 끊김
+    socket.on("close", onSocketClose);
+
+    // string 형식의 메시지 받기 
+    socket.on("message", (msg) => {
+        // 프론트로 받은 메세지를 다시 돌려주기 
+        const message = JSON.parse(msg);    //JSON으로 변환 
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickName} : ${message.payload.toString('utf8')}`)); //닉네임 프로퍼티를 socket object에 저장하고 있음 
+            case "nickName":
+                socket["nickName"] = message.payload.toString('utf8');
+        }
+    });
+});
+
+server.listen(3000, handleListen);
+
+
+{
+    type:"message";
+    payload:"hi everyone!";
+}
+
+{
+    type:"nickName";
+    payload:"anna";
+}
